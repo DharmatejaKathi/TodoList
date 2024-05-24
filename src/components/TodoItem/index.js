5cfc514d4b91fbc014f405b3677efff6ce4c0030
DharmatejaKathi <153576765+DharmatejaKathi@users.noreply.github.com>
2024-05-24 12:44:05 +0530

    Update index.js

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,20 +1,71 @@
-// Write your code here
+import {Component} from 'react'
+
 import './index.css'
 
-const TodoItem = props => {
-  const {details, onDeleteTodo} = props
-  const {title, id} = details
-  const deleteTodo = () => {
-    onDeleteTodo(id)
+class TodoItem extends Component {
+  state = {isSave: false, updatedTitle: ''}
+
+  upDateTodo = () => {
+    const {updatedTitle} = this.state
+    const {details} = this.props
+
+    this.setState({isSave: false})
   }
-  return (
-    <li className="li">
-      <p>{title}</p>
-      <button type="button" onClick={deleteTodo}>
-        Delete
-      </button>
-    </li>
-  )
+
+  onSaveTitle = () => {
+    const {details} = this.props
+    this.setState({isSave: true, updatedTitle: details.title})
+  }
+
+  onChangeTitleInput = event => {
+    this.setState({updatedTitle: event.target.value})
+  }
+
+  getTodoItem = () => {
+    const {details, onDeleteTodo} = this.props
+    const {title, id} = details
+    const deleteTodo = () => {
+      onDeleteTodo(id)
+    }
+
+    const {isSave, updatedTitle} = this.state
+
+    return (
+      <li className="li">
+        {isSave ? (
+          <>
+            <input
+              className="edit-input"
+              type="text"
+              onChange={this.onChangeTitleInput}
+              value={updatedTitle}
+            />
+            <button
+              type="button"
+              className="save-button"
+              onClick={this.upDateTodo}
+            >
+              Save
+            </button>
+          </>
+        ) : (
+          <>
+            <p className="title">{title}</p>
+            <button type="button" className="edit" onClick={this.onSaveTitle}>
+              Edit
+            </button>
+            <button type="button" onClick={deleteTodo}>
+              Delete
+            </button>
+          </>
+        )}
+      </li>
+    )
+  }
+
+  render() {
+    return <>{this.getTodoItem()}</>
+  }
 }
 
 export default TodoItem
